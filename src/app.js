8f447f2dfa66cbf0422ed68b6f7d171400ad5fb0
sean <seanwragg@gmail.com>
2015-12-04 08:31:32 -0500

    adding global cache and roster routes

--- src/app.js
+++ src/app.js
@@ -1,3 +1,5 @@
+"use strict";
+
 var restify = require('restify');
 
 var server = restify.createServer({
@@ -8,11 +10,15 @@ var server = restify.createServer({
 server.use(restify.queryParser());
 server.use(restify.bodyParser());
 
+global.cache = {};
+
 var superstars = require('./routes/superstars');
 var shows = require('./routes/shows');
+var roster = require('./routes/roster');
 
-server.get('/superstar/:slug', superstars.superstar);
-server.get('/show/:slug'. shows.show);
+server.get('/superstar/:superstar', superstars.superstar);
+server.get('/show/:show', shows.show);
+server.get('/roster/:show', roster.show);
 
 server.listen(8080, function () {
     console.log('%s listening at %s', server.name, server.url);
